c715981d29cd35d02697a63f96a38f9325778bf0
S41F4H <pittaya2545@gmail.com>
2025-04-12 19:49:18 +0700

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+import os
 import discord
 from discord.ext import commands
-import os
 
+# ดึง Token จาก Environment Variable
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'บอทออนไลน์แล้ว: {bot.user}')
+    print(f'Logged in as {bot.user}')
 
 @bot.command()
-async def ทดสอบ(ctx):
-    await ctx.send("✅ บอทพร้อมใช้งานแล้ว!")
+async def test(ctx):
+    await ctx.send("บอทพร้อมทำงานแล้ว!")
 
 bot.run(TOKEN)
